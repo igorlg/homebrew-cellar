0527c411b458cbca70d42b41d5f939005f4b3ac9
Igor Gentil <igor.lg@gmail.com>
2019-08-30 09:55:13 +1200

    Adding compiled version of Capstan as Botlle

--- Formula/capstan.rb
+++ Formula/capstan.rb
@@ -4,6 +4,12 @@ class Capstan < Formula
   url 'https://github.com/cloudius-systems/capstan/archive/v0.4.0.tar.gz'
   sha256 'ad7b86509f503926721c5564881d9f238e488e6354ab1dfa4ef30d1fa12080d2'
 
+  bottle do
+    root_url ''
+    cellar :any_skip_relocation
+    sha256 "f5e04c954626b784354bf1b3add30250ea4c121b2ea2609689f31d12baa5a476" => :mojave
+  end
+
   depends_on 'go' => :build
 
   def install
