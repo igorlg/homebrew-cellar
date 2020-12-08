1ba488d1c3a614b4b0947ffbf8fa6006abbd31c7
Igor Gentil <igorlg@amazon.com>
2020-12-08 12:08:25 +1100

    .

--- aws-proton/install.sh
+++ aws-proton/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p $HOME/.aws/models/proton-preview/2020-07-20/
-ln -s `pwd`/proton-2020-07-20.normal.json $HOME/.aws/models/proton-preview/2020-07-20/service-2.json
-ln -s `pwd`/waiters-2.json $HOME/.aws/models/proton-preview/2020-07-20/waiters-2.json
+cp proton-2020-07-20.normal.json $HOME/.aws/models/proton-preview/2020-07-20/service-2.json
+cp waiters-2.json $HOME/.aws/models/proton-preview/2020-07-20/waiters-2.json
 
