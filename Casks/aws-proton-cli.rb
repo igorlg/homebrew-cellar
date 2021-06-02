cd229f1509cd3dd13212432a78cf395c83ac532a
Igor Gentil <igorlg@amazon.com>
2021-06-03 09:27:18 +1000

    Fixed style

--- Casks/aws-proton-cli.rb
+++ Casks/aws-proton-cli.rb
@@ -1,20 +1,19 @@
-cask 'aws-proton-cli' do
-  name 'aws-proton-cli'
-  version '2020-07-20'
-  
-  sha256 '356cb52234f54eb017ba99a59eaa74fdfb6b6879a89c36ec2e185413d8b3f362'
-  url 'https://github.com/igorlg/homebrew-cellar/blob/master/aws-proton/aws-proton-preview.zip?raw=true'
+cask "aws-proton-cli" do
+  version "2020-07-20"
+  sha256 "356cb52234f54eb017ba99a59eaa74fdfb6b6879a89c36ec2e185413d8b3f362"
 
-  depends_on formula: 'awscli'
+  url "https://github.com/igorlg/homebrew-cellar/blob/master/aws-proton/aws-proton-preview.zip?raw=true"
+  name "aws-proton-cli"
 
-  model_dir = "#{ENV['HOME']}/.aws/models/proton-preview/#{version}"
+  depends_on formula: "awscli"
+
+  model_dir = "#{ENV["HOME"]}/.aws/models/proton-preview/#{version}"
+
+  installer script: "install.sh"
 
   preflight do
-    system 'unzip', 'aws-proton-preview.zip'
+    system "unzip", "aws-proton-preview.zip"
   end
 
-  installer script: 'install.sh'
-
-  uninstall delete: "#{ENV['HOME']}/.aws/models/proton-preview/"
+  uninstall delete: "#{ENV["HOME"]}/.aws/models/proton-preview/"
 end
-
--- Formula/capstan.rb
+++ Formula/capstan.rb
@@ -1,33 +1,32 @@
 class Capstan < Formula
-  desc 'Capstan CLI to manage OSv unikernels'
-  homepage 'https://github.com/cloudius-systems/capstan'
-  url 'https://github.com/cloudius-systems/capstan/archive/v0.4.0.tar.gz'
-  sha256 'ad7b86509f503926721c5564881d9f238e488e6354ab1dfa4ef30d1fa12080d2'
+  desc "CLI to manage OSv unikernels"
+  homepage "https://github.com/cloudius-systems/capstan"
+  url "https://github.com/cloudius-systems/capstan/archive/v0.4.0.tar.gz"
+  sha256 "ad7b86509f503926721c5564881d9f238e488e6354ab1dfa4ef30d1fa12080d2"
 
   bottle do
-    root_url 'https://github.com/igorlg/homebrew-cellar/releases/download/bottles'
-    cellar :any_skip_relocation
-    sha256 "f5e04c954626b784354bf1b3add30250ea4c121b2ea2609689f31d12baa5a476" => :mojave
+    root_url "https://github.com/igorlg/homebrew-cellar/releases/download/bottles"
+    sha256 cellar: :any_skip_relocation, mojave: "f5e04c954626b784354bf1b3add30250ea4c121b2ea2609689f31d12baa5a476"
   end
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath/'src'
+    ENV["GOPATH"] = buildpath/"src"
 
-    (buildpath/'src/github.com/cloudius-systems/capstan').install buildpath.children
+    (buildpath/"src/github.com/cloudius-systems/capstan").install buildpath.children
 
-    cd 'src/github.com/cloudius-systems/capstan' do
-      system 'go', 'get', '-d', '-v', '.'
-      system 'go', 'build', '-a', '-v', '-tags', 'netgo', '-o', 'bin/capstan',
-                                 '-ldflags', '"-X main.VERSION=v0.4.0 -w -s"',
-                                 'capstan.go'
-      bin.install 'bin/capstan'
+    cd "src/github.com/cloudius-systems/capstan" do
+      system "go", "get", "-d", "-v", "."
+      system "go", "build", "-a", "-v", "-tags", "netgo", "-o", "bin/capstan",
+                                 "-ldflags", '"-X main.VERSION=v0.4.0 -w -s"',
+                                 "capstan.go"
+      bin.install "bin/capstan"
       prefix.install_metafiles
     end
   end
 
   test do
-    system "#{bin}/capstan", '--help'
+    system "#{bin}/capstan", "--help"
   end
 end
