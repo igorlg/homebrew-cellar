c1630f73c93ed9e444a9f5c09edbb153acd889e4
Igor Gentil <igorlg@amazon.com>
2021-06-03 11:05:11 +1000

    Fixing aws-proton install

--- Casks/aws-proton-cli.rb
+++ Casks/aws-proton-cli.rb
@@ -1,19 +1,17 @@
 cask "aws-proton-cli" do
   version "2020-07-20"
-  sha256 "cacefe2b23629bde39e89664b10a7d72cb5298edfa05700ffa49438040795fa2"
+  sha256 "a834f7ef6e86ad531c15ca707aab1eb5d74a2760a49fb9c70b377b441c7068b7"
 
   url "https://github.com/igorlg/homebrew-cellar/blob/master/aws-proton/aws-proton-preview.zip?raw=true"
   name "aws-proton-cli"
 
   depends_on formula: "awscli"
 
-  model_dir = "#{ENV["HOME"]}/.aws/models/proton-preview/#{version}"
-
   installer script: "install.sh"
 
-  preflight do
-    system "unzip", "aws-proton-preview.zip"
-  end
+  # preflight do
+  #   system "unzip", "aws-proton-preview.zip"
+  # end
 
   uninstall delete: "#{ENV["HOME"]}/.aws/models/proton-preview/"
 end
--- aws-proton/install.sh
+++ aws-proton/install.sh
@@ -4,6 +4,8 @@
 # Install instructions from https://docs.aws.amazon.com/proton/latest/adminguide/setting-up-for-service.html#ag-setting-up-cli
 #
 
-aws configure add-model --service-model "file://proton-2020-07-20.normal.json" --service-name proton-preview
-cp waiters2.json $HOME/.aws/models/proton-preview/2020-07-20/waiters2.json
+scriptdir=`dirname "$0"`
+
+aws configure add-model --service-model "file://${scriptdir}/proton-2020-07-20.normal.json" --service-name proton-preview
+cp $scriptdir/waiters2.json $HOME/.aws/models/proton-preview/2020-07-20/waiters2.json
 
