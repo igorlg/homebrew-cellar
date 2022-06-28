3b0a6441992f15fc81f64a4daa30e4e512e791d7
Igor Gentil <igorlg@amazon.com>
2022-06-28 15:00:25 +0800

    Fixing update for aws-session-manager-plugin

--- update_packages.sh
+++ update_packages.sh
@@ -7,16 +7,16 @@ npm_formulas[1]="@mhlabs/evb-cli#evb#Formula/evb-cli.rb"
 npm_formulas[2]="@mhlabs/iam-policies-cli#iam-pol#Formula/iam-policies-cli.rb"
 npm_formulas[3]="sam-patterns-cli#sam-patterns#Formula/sam-patterns-cli.rb"
 
-for i in "${npm_formulas[@]}"; do
-  npmpkg="$(echo $i  | cut -d '#' -f 1)"
-  cmd="$(echo $i     | cut -d '#' -f 2)"
-  formula="$(echo $i | cut -d '#' -f 3)"
+# for i in "${npm_formulas[@]}"; do
+#   npmpkg="$(echo $i  | cut -d '#' -f 1)"
+#   cmd="$(echo $i     | cut -d '#' -f 2)"
+#   formula="$(echo $i | cut -d '#' -f 3)"
 
-  echo "Updating ${npmpkg} (${formula}). Test cmd: ${cmd}"
-  noob $npmpkg \
-  | sed "s/raise \"Test.*/system \"#{bin}\/${cmd}\", \"-v\"/g" \
-  | tee $formula
-done
+#   echo "Updating ${npmpkg} (${formula}). Test cmd: ${cmd}"
+#   noob $npmpkg \
+#   | sed "s/raise \"Test.*/system \"#{bin}\/${cmd}\", \"-v\"/g" \
+#   | tee $formula
+# done
 
 
 echo "Getting latest version of aws-session-manager-plugin"
@@ -27,13 +27,17 @@ if [[ $ssm_plugin_current == $ssm_plugin_latest ]]; then
   exit 0
 fi
 
-echo "Downloading session-manager-plugin v${ssm_plugin_latest} to calculate sha256sum"
-wget -q "https://github.com/aws/session-manager-plugin/archive/refs/tags/${ssm_plugin_latest}.zip"
-ssm_sha_latest="$(sha256sum ${ssm_plugin_latest}.zip | awk '{ print $1 }')"
-ssm_sha_current="$(grep '  sha256' Formula/aws-session-manager-plugin.rb | awk '{ print $2 }' | tr -d '\"')"
-rm "${ssm_plugin_latest}.zip"
-
 formula="Formula/aws-session-manager-plugin.rb"
+echo "Downloading session-manager-plugin v${ssm_plugin_latest} to calculate sha256sum"
+wget -q "https://s3.amazonaws.com/session-manager-downloads/plugin/${ssm_plugin_latest}/mac/sessionmanager-bundle.zip"
+
+ssm_sha_latest="$(sha256sum sessionmanager-bundle.zip | awk '{ print $1 }')"
+ssm_sha_current="$(grep '  sha256' $formula | awk '{ print $2 }' | tr -d '\"')"
+
+rm "sessionmanager-bundle.zip"
+
+echo "[aws-session-manager-plugin] ssm_sha_latest:  ${ssm_sha_latest}"
+echo "[aws-session-manager-plugin] ssm_sha_current: ${ssm_sha_current}"
 
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "Updating $formula using MacOS SED"
