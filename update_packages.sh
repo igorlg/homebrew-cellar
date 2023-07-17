40246f2cbb1156d0266a3dc43f3ace41166ebb40
Igor Gentil <igorlg@amazon.com>
2023-07-17 11:14:40 +1000

    chore: update script to reflect formula changes

--- update_packages.sh
+++ update_packages.sh
@@ -5,7 +5,7 @@ declare -a npm_formulas
 npm_formulas[0]="@mhlabs/cwlogs-cli#cwlogs#Formula/cwlogs-cli.rb"
 npm_formulas[1]="@mhlabs/evb-cli#evb#Formula/evb-cli.rb"
 npm_formulas[2]="@mhlabs/iam-policies-cli#iam-pol#Formula/iam-policies-cli.rb"
-npm_formulas[3]="sam-patterns-cli#sam-patterns#Formula/samp-cli.rb"
+npm_formulas[3]="samp-cli#samp#Formula/samp-cli.rb"
 
 for i in "${npm_formulas[@]}"; do
   npmpkg="$(echo $i  | cut -d '#' -f 1)"
@@ -18,34 +18,3 @@ for i in "${npm_formulas[@]}"; do
   | sed "s/class Mhlabs/class /g" \
   | tee $formula
 done
-
-
-echo "Getting latest version of aws-session-manager-plugin"
-ssm_plugin_latest="$(gh release list -R aws/session-manager-plugin -L 1 | awk '{ print $10 }' | tr -d '()')"
-ssm_plugin_current="$(grep '  version' Formula/aws-session-manager-plugin.rb | awk '{ print $2 }' | tr -d '\"')"
-if [[ $ssm_plugin_current == $ssm_plugin_latest ]]; then
-  echo "Skipping update of session-manager-plugin (Formula/aws-session-manager-plugin.rb)"
-  exit 0
-fi
-
-formula="Formula/aws-session-manager-plugin.rb"
-echo "Downloading session-manager-plugin v${ssm_plugin_latest} to calculate sha256sum"
-wget -q "https://s3.amazonaws.com/session-manager-downloads/plugin/${ssm_plugin_latest}/mac/sessionmanager-bundle.zip"
-
-ssm_sha_latest="$(sha256sum sessionmanager-bundle.zip | awk '{ print $1 }')"
-ssm_sha_current="$(grep '  sha256' $formula | awk '{ print $2 }' | tr -d '\"')"
-
-rm "sessionmanager-bundle.zip"
-
-echo "[aws-session-manager-plugin] ssm_sha_latest:  ${ssm_sha_latest}"
-echo "[aws-session-manager-plugin] ssm_sha_current: ${ssm_sha_current}"
-
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "Updating $formula using MacOS SED"
-  sed -i '' "s/${ssm_sha_current}/${ssm_sha_latest}/g" $formula
-  sed -i '' "s/${ssm_plugin_current}/${ssm_plugin_latest}/g" $formula
-else
-  echo "Updating $formula using Linux SED"
-  sed -i "s/${ssm_sha_current}/${ssm_sha_latest}/g" $formula
-  sed -i "s/${ssm_plugin_current}/${ssm_plugin_latest}/g" $formula
-fi
