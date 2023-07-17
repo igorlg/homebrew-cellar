68194b16759795a401d84b353d3b75c05f899790
Igor Gentil <igorlg@amazon.com>
2023-07-17 11:05:59 +1000

    chore: remove formulas superseeded by original maintainers, or of deprecated packages

--- update_packages.sh
+++ update_packages.sh
@@ -5,8 +5,7 @@ declare -a npm_formulas
 npm_formulas[0]="@mhlabs/cwlogs-cli#cwlogs#Formula/cwlogs-cli.rb"
 npm_formulas[1]="@mhlabs/evb-cli#evb#Formula/evb-cli.rb"
 npm_formulas[2]="@mhlabs/iam-policies-cli#iam-pol#Formula/iam-policies-cli.rb"
-npm_formulas[3]="sam-patterns-cli#sam-patterns#Formula/sam-patterns-cli.rb"
-npm_formulas[4]="artillery#artillery#Formula/artillery.rb"
+npm_formulas[3]="sam-patterns-cli#sam-patterns#Formula/samp-cli.rb"
 
 for i in "${npm_formulas[@]}"; do
   npmpkg="$(echo $i  | cut -d '#' -f 1)"
